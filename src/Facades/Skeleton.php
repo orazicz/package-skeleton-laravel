2f7c24f16749ee8983aec701c0d47530570310d0
Jakub Orava <Jakuborava@gmail.com>
2024-06-19 12:39:53 +0200

    Update of Laravel package skeleton

--- src/Facades/Skeleton.php
+++ src/Facades/Skeleton.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Facade;
  */
 class Skeleton extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return \Orazicz\Skeleton\Skeleton::class;
     }
